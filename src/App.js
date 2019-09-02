7dd280b0f02387e51b8fbf47b346bb45b1d89b83
scode97 <sehameldeen1997@gmail.com>
2019-09-03 00:01:59 +0300

    tasks_basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import tasks from "./data";
 
+import "font-awesome/css/font-awesome.min.css";
 function App() {
+  // const statusIcon = done => {
+  //   if (done) {
+  //     return <i className="fa fa-check-circle" />;
+  //   } else {
+  //     return <i className="fa fa-times-circle" />;
+  //   }
+  // };
+  // calling for above
+  // {/* <td>{statusIcon(tasks[0].done)}</td> */}
+  //or
+
+  const statusIcon = asis => {
+    if (asis) return "fa fa-check-circle";
+    return "fa fa-times-circle";
+  };
+
+  // let icon = <i className="fa fa-check-circle" />
+  console.log(tasks);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="rectangle">
+      <p className="title">TO DO LIST</p>
+      <table>
+        <thead>
+          <tr>
+            <th>STATUS</th>
+            <th>TASK</th>
+            <th>PRIORITY</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <i className={statusIcon(tasks[0].done)} />
+            </td>
+            <td>{tasks[0].task}</td>
+            <td className={tasks[0].priority}>
+              {tasks[0].priority.toUpperCase()}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <i className={statusIcon(tasks[1].done)} />
+            </td>
+            <td>{tasks[1].task}</td>
+            <td className={tasks[1].priority}>
+              {tasks[1].priority.toUpperCase()}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <i className={statusIcon(tasks[2].done)} />
+            </td>
+            <td>{tasks[2].task}</td>
+            <td className={tasks[2].priority}>
+              {tasks[2].priority.toUpperCase()}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+let tasks = [
+  {
+    task: "Go to the supermarket",
+    done: true,
+    priority: "middle"
+  },
+  {
+    task: "Cook dinner",
+    done: false,
+    priority: "middle"
+  },
+  {
+    task: "Call Man",
+    done: false,
+    priority: "low"
+  }
+];
+
+export default tasks;
